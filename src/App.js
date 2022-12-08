7f8252d6161dbfb99a5c03be38055c0e5222b653
demiid2 <demiandev2@gmail.com>
2022-12-08 19:44:32 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+
+
+function App(props) {
+  const [datos, setDatos] = useState([]);
+  useEffect(() => {
+      axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${props.palabra}`)
+      .then(response => setDatos(response.data));
+      console.log(datos)
+    
+  }, [])
+ // let first = datos[0]
+ // let definition = first.meanings[0].definitions[1].definition;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>{datos.word}</h1>
+     
     </div>
   );
 }
